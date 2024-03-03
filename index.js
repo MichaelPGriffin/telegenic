e85a732a89b9bd963e1282097b2180520c9f531f
Michael Griffin <mpgriffin5@gmail.com>
2024-03-02 22:27:57 -0800

    Add support for `.tar.gz` installations

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const fs = require('fs');
 const uuid = require('uuid');
 const multer = require('multer');
 const unzipper = require('unzipper');
+const tar = require('tar');
+const stream = require('stream');
+const zlib = require('zlib');
+const  { tmpdir } = require('node:os');
+const  { sep } = require('node:path');
 const { exec } = require('child_process');
 
 
@@ -10,7 +15,7 @@ const app = express();
 const port = 3001;
 app.use(express.text());
 app.use(express.json());
-express.urlencoded()
+express.urlencoded();
 
 
 app.listen(port, () => {
@@ -90,30 +95,97 @@ const installZipProgram = async (req, res) => {
     return res.status(400).json({ error: 'No file uploaded.' });
   }
 
-  try {
-    const id = uuid.v1();
-    const extractionDir = `./installations/${id}`;
-    const zipDir = await unzipper.Open.buffer(file.buffer);
-    const { files } = zipDir;
+  const id = uuid.v1();
 
-    if (!files || !files.length) {
-      return res.status(400).json({ error: 'Empty zip' });
+  try {
+    const extractionDir = `./installations/${id}`;
+
+    const fileExtension = fileName => {
+      const zip = '.zip';
+      const tarGz = '.tar.gz';
+
+      if (fileName.endsWith(zip)) {
+        return zip;
+      }
+
+      if (fileName.endsWith(tarGz)) {
+        return tarGz;
+      }
+
+      throw new Error(`Unsupported file extension in file name ${fileName}`);
     }
 
-    const writes = files.map(async file => {
-      const filePath = `${extractionDir}/${file.path}`
-      const directoryPath = filePath.substring(0, filePath.lastIndexOf('/'));
-      fs.mkdir(directoryPath, { recursive: true }, () => {
-        file.stream().pipe(fs.createWriteStream(filePath));
-      });
-    });
+    let files = null;
+    if (fileExtension(file.originalname) == '.zip') {
+      const zipDir = await unzipper.Open.buffer(file.buffer);
+      files = zipDir.files;
 
-    await Promise.all(writes);
-    exec('yarn', { cwd: extractionDir });
-    res.send(`Progrm UUID\n${id}`);
+      if (!files || !files.length) {
+        return res.status(400).json({ error: 'Empty zip' });
+      }
+
+      const writes = files.map(async file => {
+        const filePath = `${extractionDir}/${file.path}`;
+        const directoryPath = filePath.substring(0, filePath.lastIndexOf('/'));
+        fs.mkdir(directoryPath, { recursive: true }, () => {
+          file.stream().pipe(fs.createWriteStream(filePath));
+        });
+      });
+
+      await Promise.all(writes);
+      exec('yarn', { cwd: extractionDir });
+      res.send(`Progrm UUID\n${id}`);
+    } else if (fileExtension(file.originalname) == '.tar.gz') {
+
+      if (!file || !file.size) {
+        return res.status(400).json({ error: 'Empty tar.gz' });
+      }
+
+      fs.mkdir(extractionDir, () => {
+        // This assumes the tar.gz contents were in a folder, and that folder was the input to the  `tar` command.
+        const tmpDir = tmpdir();
+        fs.mkdtemp(`${tmpDir}${sep}`, (err, tmpLocation) => {
+          if (err) {
+            throw err;
+          }
+
+          const tarGzReadStream = stream.Readable.from(file.buffer);
+          const gunzipStream = zlib.createGunzip();
+          const tarExtractStream = tar.extract({ cwd: tmpLocation });
+          tarGzReadStream.pipe(gunzipStream).pipe(tarExtractStream);
+
+          tarExtractStream.on('finish', () => {            
+            fs.readdir(tmpLocation, (err, files) => {
+              if (err) {
+                throw err;
+              }
+
+              try {
+                const extensionIndex = file.originalname.lastIndexOf('.tar.gz');
+                const intermediateFolderName = file.originalname.substring(0, extensionIndex);
+
+                fs.readdirSync(`${tmpLocation}${sep}${intermediateFolderName}`)
+                .forEach(f => {
+                  fs.copyFileSync(`${tmpLocation}${sep}${intermediateFolderName}${sep}${f}`, `${extractionDir}${sep}${f}`)
+                });
+                exec('yarn', { cwd: extractionDir });
+                res.send(`Progrm UUID\n${id}`);
+              } catch (err) {
+                console.log(err);
+                res.send(err);
+              } finally {
+                fs.rm(tmpLocation, { recursive: true, force: true }, () => {});
+              }
+            });
+          });
+
+          tarExtractStream.on('error', err => { throw err; });
+        });
+      });
+    }
   } catch (err) {
     console.log('Err:');
-    console.log(JSON.stringify(err));
+    console.log(err);
     res.send(`Error installing program UUID\n${id}`);
   }
 }
